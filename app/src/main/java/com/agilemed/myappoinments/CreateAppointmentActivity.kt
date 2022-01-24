80018883300ee9131fcda4f860031110bbd830b2
oswaldomercado26 <79757745+oswaldomercado26@users.noreply.github.com>
2022-01-24 14:36:24 -0600

    back to step 1 and validate

--- app/src/main/java/com/agilemed/myappoinments/CreateAppointmentActivity.kt
+++ app/src/main/java/com/agilemed/myappoinments/CreateAppointmentActivity.kt
@@ -8,6 +8,9 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_create_appointment.*
 import kotlinx.android.synthetic.main.card_view_step_one.*
 import kotlinx.android.synthetic.main.card_view_step_three.*
 import kotlinx.android.synthetic.main.card_view_step_two.*
@@ -21,9 +24,33 @@ class CreateAppointmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_appointment)
 
-        btnNext.setOnClickListener {
+     btnNext.setOnClickListener {
+         if (etDescription.text.toString().length < 3) {
+             etDescription.error = getString(R.string.validate_appointment_description)
+         } else {
+             // continue to step 2
+             cvStep1.visibility = View.GONE
+             cvStep2.visibility = View.VISIBLE
+         }
+     }
 
-        }
+     btnNext2.setOnClickListener {
+         when {
+             etScheduledDate.text.toString().isEmpty() ->
+                 etScheduledDate.error = getString(R.string.validate_appointment_date)
+
+             selectedTimeRadioBtn == null ->
+                 Snackbar.make(createAppointmentLinearLayout,
+                     R.string.validate_appointment_time, Snackbar.LENGTH_SHORT).show()
+
+             else -> {
+                 // continue to step 3
+                 //showAppointmentDataToConfirm()
+                 cvStep2.visibility = View.GONE
+                 cvStep3.visibility = View.VISIBLE
+             }
+         }
+     }
 
         btnConfirmAppointment.setOnClickListener {
             Toast.makeText(this,"Cita registrar correctamente", Toast.LENGTH_SHORT).show()
@@ -93,10 +120,41 @@ class CreateAppointmentActivity : AppCompatActivity() {
                 radioGroupLeft.addView(radioButton)
             else
                 radioGroupRight.addView(radioButton)
-                goToLeft = !goToLeft
+            goToLeft = !goToLeft
         }
     }
 
     private fun Int.twoDigits()
             = if (this>=10) this.toString() else "0$this"
+
+    override fun onBackPressed() {
+
+        when {
+            cvStep3.visibility == View.VISIBLE -> {
+                cvStep3.visibility = View.GONE
+                cvStep2.visibility = View.VISIBLE
+
+            }
+            cvStep2.visibility == View.VISIBLE -> {
+                cvStep2.visibility = View.GONE
+                cvStep1.visibility = View.VISIBLE
+
+            }
+            cvStep1.visibility == View.VISIBLE -> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(getString(R.string.dialog_create_appointment_exit_title))
+                builder.setMessage(getString(R.string.dialog_create_appointment_exit_message))
+                builder.setPositiveButton(getString(R.string.dialog_create_appointment_exit_positive_btn)) { _, _ ->
+                    finish()
+                }
+                builder.setNegativeButton(getString(R.string.dialog_create_appointment_exit_negative_btn)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+
+                val dialog = builder.create()
+                dialog.show()
+            }
+        }
+
+    }
 }
